961346f937c2423f7027632ee0412b3c820965b8
wsfer <93054415+wsfer@users.noreply.github.com>
2025-12-30 16:52:14 -0300

    feat: create login controller function

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,16 +1,59 @@
+import type { User } from '../generated/prisma/client';
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { prisma } from '../lib/prisma';
 import validateUser from '../middlewares/validateUser';
+import validateLogin from '../middlewares/validateLogin';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'to_shut_up_typescript';
+const JWT_EXPIRATION_TIME = 60 * 120; // 2h
 
 function getProfile(req: Request, res: Response) {
   res.status(418).end();
 }
 
-function postLogin(req: Request, res: Response) {
-  res.status(418).end();
-}
+const postLogin = [
+  validateLogin,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.mapped());
+    }
+
+    const { username, password } = req.body;
+    const user: User | null = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    // User not found
+    if (!user) {
+      return res.status(401).json({ error: 'Incorrect username or password' });
+    }
+
+    const matchPassword: boolean = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    // Incorrect password
+    if (!matchPassword) {
+      return res.status(401).json({ error: 'Incorrect username or password' });
+    }
+
+    jwt.sign(
+      user,
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRATION_TIME },
+      (err, token) => {
+        if (err) throw err;
+        return res.json({ token });
+      }
+    );
+  },
+];
 
 const postRegister = [
   validateUser,
