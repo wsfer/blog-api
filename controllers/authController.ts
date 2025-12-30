27c4263d95eb36d73c3eda348125cbcd54a2fc73
wsfer <93054415+wsfer@users.noreply.github.com>
2025-12-30 15:33:28 -0300

    feat: add controller function to register a new user

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+import { prisma } from '../lib/prisma';
+import validateUser from '../middlewares/validateUser';
+import bcrypt from 'bcryptjs';
 
 function getProfile(req: Request, res: Response) {
   res.status(418).end();
@@ -8,9 +12,28 @@ function postLogin(req: Request, res: Response) {
   res.status(418).end();
 }
 
-function postRegister(req: Request, res: Response) {
-  res.status(418).end();
-}
+const postRegister = [
+  validateUser,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.mapped());
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      },
+    });
+
+    // TODO: sign in jwt
+    res.status(201).json(newUser);
+  },
+];
 
 function postLogout(req: Request, res: Response) {
   res.status(418).end();
