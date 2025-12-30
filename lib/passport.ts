d30fb1fe4baae0428a00254b88bfc6dbb5b9ac9e
wsfer <93054415+wsfer@users.noreply.github.com>
2025-12-30 18:08:00 -0300

    feat: implement passport.js jwt authentication

--- lib/passport.ts
+++ lib/passport.ts
@@ -1,17 +1,28 @@
-import passport from 'passport';
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import type { StrategyOptions } from 'passport-jwt';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { prisma } from './prisma';
+import passport from 'passport';
 
 const opts: StrategyOptions = {
-  secretOrKey: '123',
+  secretOrKey: process.env.JWT_SECRET || 'to_shut_up_typescript',
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 };
 
 passport.use(
-  new JwtStrategy(opts, (jwtPayload, done) => {
-    // TODO: fetch user from db using jwtPayload.id
-    // then check if exists
-    done(null, false);
+  new JwtStrategy(opts, async (jwtPayload, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: jwtPayload.id },
+      });
+
+      if (user) {
+        return done(false, user);
+      }
+
+      return done(null, false);
+    } catch (err) {
+      return done(err, false);
+    }
   })
 );
 
