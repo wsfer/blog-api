d020e104a14f44b69495a3468550b0bbffd97da6
wsfer <93054415+wsfer@users.noreply.github.com>
2025-12-30 17:43:38 -0300

    refactor: move form validation middlewares from controller to router

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -2,8 +2,6 @@ import type { User } from '../generated/prisma/client';
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { prisma } from '../lib/prisma';
-import validateRegister from '../middlewares/validateRegister';
-import validateLogin from '../middlewares/validateLogin';
 import signInUser from '../utils/signInUser';
 import bcrypt from 'bcryptjs';
 
@@ -11,62 +9,53 @@ function getProfile(req: Request, res: Response) {
   res.status(418).end();
 }
 
-const postLogin = [
-  validateLogin,
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+async function postLogin(req: Request, res: Response) {
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.mapped());
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.mapped());
+  }
 
-    const { username, password } = req.body;
-    const user: User | null = await prisma.user.findUnique({
-      where: { username },
-    });
+  const { username, password } = req.body;
+  const user: User | null = await prisma.user.findUnique({
+    where: { username },
+  });
 
-    // User not found
-    if (!user) {
-      return res.status(401).json({ error: 'Incorrect username or password' });
-    }
+  // User not found
+  if (!user) {
+    return res.status(401).json({ error: 'Incorrect username or password' });
+  }
 
-    const matchPassword: boolean = await bcrypt.compare(
-      password,
-      user.password
-    );
+  const matchPassword: boolean = await bcrypt.compare(password, user.password);
 
-    // Incorrect password
-    if (!matchPassword) {
-      return res.status(401).json({ error: 'Incorrect username or password' });
-    }
+  // Incorrect password
+  if (!matchPassword) {
+    return res.status(401).json({ error: 'Incorrect username or password' });
+  }
 
-    const token = await signInUser(user);
-    return res.json({ token });
-  },
-];
+  const token = await signInUser(user);
+  return res.json({ token });
+}
 
-const postRegister = [
-  validateRegister,
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+async function postRegister(req: Request, res: Response) {
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.mapped());
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.mapped());
+  }
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const newUser = await prisma.user.create({
-      data: {
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword,
-      },
-    });
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const newUser = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    },
+  });
 
-    const token = await signInUser(newUser);
-    return res.json({ token });
-  },
-];
+  const token = await signInUser(newUser);
+  return res.json({ token });
+}
 
 function postLogout(req: Request, res: Response) {
   res.status(418).end();
--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import authController from '../controllers/authController';
+import validateLogin from '../middlewares/validateLogin';
+import validateRegister from '../middlewares/validateRegister';
 
 const authRouter = Router();
 
 authRouter.get('/profile', authController.getProfile);
-authRouter.post('/login', authController.postLogin);
-authRouter.post('/register', authController.postRegister);
+authRouter.post('/login', validateLogin, authController.postLogin);
+authRouter.post('/register', validateRegister, authController.postRegister);
 authRouter.post('/logout', authController.postLogout);
 
 export default authRouter;
