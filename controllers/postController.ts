2f99272c9fe256e9c012b88740488679532bd3cc
wsfer <93054415+wsfer@users.noreply.github.com>
2026-01-12 17:04:52 -0300

    feat: add feature to create a post

--- controllers/postController.ts
+++ controllers/postController.ts
@@ -1,4 +1,6 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
+import type { User } from '../generated/prisma/client';
+import { prisma } from '../lib/prisma';
 
 function getPosts(req: Request, res: Response) {
   res.status(418).end();
@@ -8,8 +10,18 @@ function getPost(req: Request, res: Response) {
   res.status(418).end();
 }
 
-function postPost(req: Request, res: Response) {
-  res.status(418).end();
+async function postPost(req: Request, res: Response) {
+  const newPost = await prisma.post.create({
+    data: {
+      title: req.body.title,
+      content: req.body.content,
+      owner: {
+        connect: { id: (req.user as User).id },
+      },
+    },
+  });
+
+  res.json(newPost);
 }
 
 function updatePost(req: Request, res: Response) {
--- routes/postRouter.ts
+++ routes/postRouter.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
 import postController from '../controllers/postController';
 import commentController from '../controllers/commentController';
+import validatePost from '../middlewares/validatePost';
+import authenticateUser from '../middlewares/authenticateUser';
+import handleFormErrors from '../middlewares/handleFormErrors';
 
 const postRouter = Router();
 
 postRouter.get('/', postController.getPosts);
 postRouter.get('/:postId', postController.getPost);
 postRouter.get('/:postId/comments', commentController.getPostComments);
-postRouter.post('/', postController.postPost);
+
+postRouter.post(
+  '/',
+  authenticateUser(['ADMIN']),
+  validatePost,
+  handleFormErrors,
+  postController.postPost
+);
+
 postRouter.patch('/:postId', postController.updatePost);
 postRouter.delete('/:postId', postController.deletePost);
 
